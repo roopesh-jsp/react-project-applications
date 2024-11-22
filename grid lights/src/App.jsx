e975db40de35a2ef01d63d3d6d85c8c2d92a8332
roopesh-jsp <rupzkumar5@gmail.com>
2024-11-22 16:19:45 +0530

    done

--- grid lights/src/App.jsx
+++ grid lights/src/App.jsx
@@ -5,7 +5,8 @@ const config = [
   [1, 1, 1],
   [1, 0, 1],
   [1, 1, 1],
+  [0, 0, 1],
 ];
 export default function App() {
-  return <GridLights grids={config.flat()} />;
+  return <GridLights grids={config} />;
 }
--- grid lights/src/components/GridLights.jsx
+++ grid lights/src/components/GridLights.jsx
@@ -1,39 +1,45 @@
 import React, { useState } from "react";
 
 export default function GridLights({ grids }) {
-  const [order, setOrder] = useState([]);
-  function deleselecting() {
+  const [queue, setQueue] = useState([]);
+  function handleClick(idx) {
+    if (queue.includes(idx)) {
+      return;
+    }
+    setQueue((prev) => [...prev, idx]);
+
+    if (queue.length + 1 === grids.flat().filter(Boolean).length) {
+      removeLights();
+    }
+  }
+  function removeLights() {
     const timer = setInterval(() => {
-      setOrder((prev) => {
+      setQueue((prev) => {
         const newOrder = prev.slice();
         newOrder.pop();
+
         if (newOrder.length === 0) {
           clearInterval(timer);
         }
+
         return newOrder;
       });
     }, 300);
   }
-  function handleClick(idx) {
-    if (order.includes(idx)) {
-      return;
-    }
-    let newOrder = [...order, idx];
-    setOrder(newOrder);
-    if (newOrder.length === grids.filter(Boolean).length) {
-      newOrder = [];
-      deleselecting();
-    }
-  }
   return (
-    <div className="container">
-      {grids.map((value, idx) => {
-        return value ? (
-          <button
+    <div
+      className="container"
+      style={{
+        gridTemplateColumns: `repeat(${grids[0].length},1fr)`,
+      }}
+    >
+      {grids.flat().map((ele, idx) => {
+        return ele ? (
+          <div
             key={idx}
-            className={`grid ${order.includes(idx) && "selected"}`}
+            className={`grid ${queue.includes(idx) ? "selected" : ""}`}
             onClick={() => handleClick(idx)}
-          />
+          ></div>
         ) : (
           <span key={idx}></span>
         );
