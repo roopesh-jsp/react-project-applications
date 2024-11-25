4c8f1d3f6dc7029564f7a63189904832b357166b
roopesh-jsp <rupzkumar5@gmail.com>
2024-11-25 11:26:26 +0530

    done hasMore state to be added

--- infinite scrolling/src/App.jsx
+++ infinite scrolling/src/App.jsx
@@ -1,21 +1,60 @@
-import React, { useState } from "react";
-import useBookSearch from "./useBookSearch";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function App() {
-  const [query, setQuery] = useState("");
-  const [pgNo, setPgNo] = useState(1);
-  console.log("hai");
-  function handleQuery(e) {
-    setQuery(e.target.value);
-    setPgNo(1);
-  }
-  const { books, loading, hasMore } = useBookSearch(query, pgNo);
-  console.log(books);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  async function getData() {
+    setLoading(true);
+    const res = await axios({
+      url: "https://jsonplaceholder.typicode.com/posts",
+      method: "get",
+      params: {
+        _page: page,
+        _limit: 10,
+      },
+    });
+    setData((prev) => [...prev, ...res.data]);
+    console.log(res.data);
 
+    setLoading(false);
+  }
+  async function handleNewPageLoad() {
+    // console.log(document.documentElement.scrollHeight);
+    // console.log(document.documentElement.scrollTop);
+    // console.log(window.innerHeight);
+    try {
+      if (
+        document.documentElement.scrollTop + window.innerHeight >=
+        document.documentElement.scrollHeight
+      ) {
+        setPage((prev) => prev + 1);
+        // check for hasMore !! ??
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    getData();
+  }, [page]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleNewPageLoad);
+  }, []);
   return (
     <div>
-      <input type="text" placeholder="enter" onChange={handleQuery} />
-      {loading && <p>loading...</p>}
+      {data.map((ele, idx) => {
+        return (
+          <div key={idx} className="card">
+            <h1>{ele.title}</h1>
+            <p>{ele.body}</p>
+            <span>user :{ele.userId}</span>
+          </div>
+        );
+      })}
+      {loading && <h1 className="loading">loading....</h1>}
     </div>
   );
 }
