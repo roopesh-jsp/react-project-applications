a4b67c894f95d471f7b0e68fbff9ad3b0a12d6aa
roopesh-jsp <rupzkumar5@gmail.com>
2024-11-30 21:47:03 +0530

    basic syntaxes covered

---
+++ sass/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>haii</h1>
+      <div className="div">
+        <div id="div">
+          <span>
+            {" "}
+            this has a <button>click me</button>
+          </span>
+          <span>hai</span>
+        </div>
+      </div>
+    </div>
+  );
+}
